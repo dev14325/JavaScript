bd129b2009b14eba694293f5c532446e5cab6d4a
Dev Pratap Singh <devthakur31931@gmail.com>
2024-01-12 17:29:33 +0530

    learning callbacks in js

---
+++ class&objects/practice.js
@@ -0,0 +1,13 @@
+class user{
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData(){
+        console.log('Data is visible');
+    }
+}
+
+let s1 = new user('Dev','devthakur31931@gamil.com');
+let s2 = new user('Beast','beast2.0@gmail.com');
